a8671059ab6aea7835a1c492554c903c529959a3
Sergey Skuridin <skuridin@gmail.com>
2023-05-03 19:20:27 +0300

    fix array_max.py

--- array_max.py
+++ array_max.py
@@ -1,17 +1,17 @@
 import random
 current = list(range(16))
-#current = random.randint(0, 16)
+current = random.randint(0, 16)
 print(current)
-#current =  [6, 12, 4, 3, 8]
+# current = [6, 12, 4, 3, 8]
 max_index=0
-n=len(current)
-i=0
-max = current[0]
-while (i < n):
-    if max < current[i]:
-        max = current[i]
+n = len(current)
+i = 0
+max_el = current[0]
+while i < n:
+    if max_el < current[i]:
+        max_el = current[i]
         max_index=i
-    i+=1
-print("Максимальный элемент -", max, "индекс максимального элемента -", max_index)
+    i += 1
+print("Максимальный элемент -", max_el, "индекс максимального элемента -", max_index)
 
     
