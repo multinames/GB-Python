f3aac824b3c8fb4043a5bb9906f2829e754b6747
Sergey Skuridin <skuridin@gmail.com>
2023-05-03 19:12:57 +0300

    fix array2.py

--- gb-lesson/array2.py
+++ gb-lesson/array2.py
@@ -2,12 +2,12 @@
 import random
 current = list(range(16))
 random.shuffle(current)
-n=len(current)
+n = len(current)
 mirror = [0 for i in range(n)]
 i = 0
 print("Исходный массив", current)
-while i < len (current) :
-    mirror[i]=current[n-1]
-    i=i+1
-    n=n-1
+while i < len (current):
+    mirror[i] = current[n-1]
+    i = i+1
+    n = n-1
 (print("Перевернутый массив", mirror))
